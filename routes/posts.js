8f49c02299bd09e2f27d53368e66d4897e9490e9
PrashantNauhwar <nauhwarprashant30@gmail.com>
2022-07-17 01:58:01 +0530

    DELETE  and Get post api

--- routes/posts.js
+++ routes/posts.js
@@ -40,10 +40,41 @@ router.put("/:id",async(req,res)=>{
 
 
 // DELETE POST 
+router.delete("/:id",async(req,res)=> {
 
+    try{
+
+        const post = await Post.findById(req.params.id);
+        if(post.username == req.body.username) {
+            try {
+                await post.delete();
+                res.status(200).json("Post has been deleted....")
+            }catch(err) {
+                res.status(500).json(err)
+            }
+        } else{
+            res.status(401).json("you can only delete your post")
+        }
+        
+    }catch(err) {
+        res.status(500).json(err)
+    }
+
+})
 
 
 // GET POST 
+router.get("/:id",async(req,res)=> {
+    try {
+
+        const post = await Post.findById(req.params.id);
+
+        res.status(200).json(post);        
+
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 
 
